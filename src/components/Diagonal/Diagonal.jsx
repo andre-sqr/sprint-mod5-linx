fed53dfef43ca78a55f88c94f4b9644a79d60e49
andre-sqr <andrebsqr@gmail.com>
2022-08-17 21:53:03 -0300

    add: componente diagonal

---
+++ src/components/Diagonal/Diagonal.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from './Diagonal.module.css'
+
+const Diagonal = () => {
+  return (
+    <div className={Style.diagonal}></div>
+  )
+}
+
+export default Diagonal
