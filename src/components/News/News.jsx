b8e460cbb047f58c916ce2a561beb2877b85836c
andre-sqr <andrebsqr@gmail.com>
2022-08-22 18:17:07 -0300

    alterando estilo do componente news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import Button from '../Button/Button'
+import Style from './News.module.css'
 
 const News = () => {
   return (
-    <div>
-        <h1>Compartilhe as novidades</h1>
-        <h2>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</h2>
-        <div>
-        <label>Nome do seu amigo:</label>
-        <input type="text"></input>
-        <label>E-mail:</label>
-        <input type="email"></input>
+    <div className={Style.all}>
+        <h1 className={Style.h1}>Compartilhe as novidades</h1>
+        <h2 className={Style.h2}>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</h2>
+        <div className={Style.sectionRow}>
+          <div className={Style.sectionColumn}>
+          <label>Nome do seu amigo:</label>
+          <input type="text" className={Style.input}></input>
+          </div>
+          <div className={Style.sectionColumn}>
+            <label>E-mail:</label>
+            <input type="email" className={Style.input}></input>
+          </div>
         </div>
-        <button>Enviar agora</button>
+        <Button className={Style.btn} texto="Enviar agora"/>
     </div>
   )
 }
